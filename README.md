dc6b67c02e92ca164c57ed6483350abca30de3ba
GauravSingh75 <55052983+GauravSingh9356@users.noreply.github.com>
2020-06-08 20:53:56 +0530

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1>Path to hosts File</h1>
+<h2>In Windows</h2>
+
+```
+
+"C:\windows\system32\drivers\etc\hosts
+
+```
+
+<h2>In Linux</h2>
+
+```
+"etc\hosts"
+
+```
+
+<h3>Overview of Working of program</h3>
+
+<p>It uses hosts of operating system which can be used to block certain websites in certain times whenever user doesn't want that website to run even though user tries to browse those sites as this is is user's working hours set by user in python file.</p>
---
+++ website_block.py
@@ -0,0 +1,31 @@
+# for windows host file is in "C:\windows\system32\drivers\etc\hosts"
+# for linux host file is in "etc\hosts"
+
+
+import time
+from datetime import datetime as dt
+hosts_temp = "hosts"
+redirect = "127.0.0.1"
+website_list = ["instagram.com", "www.instagram.com",
+                "facebook.com", "www.facebook.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 16) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
+        print("Working Hours...")
+        with open(hosts_temp, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+    else:
+        with open(hosts_temp, "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun Hours!!")
+    time.sleep(5)
